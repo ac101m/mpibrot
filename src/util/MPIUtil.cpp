6292ea5a1ec2cbad401b281df44787d5ca75acab
ac101m <mofty101@gmail.com>
2019-11-12 16:20:27 +0000

    Remove util::mpi::Tag

--- src/util/MPIUtil.cpp
+++ src/util/MPIUtil.cpp
@@ -8,11 +8,6 @@
 #include "MPIUtil.hpp"
 
 
-// What range of tags to initialise free tag list with
-#define MPIUTIL_TAG_RANGE_START 1024
-#define MPIUTIL_TAG_RANGE_END 2047
-
-
 namespace util
 {
   namespace mpi
@@ -32,10 +27,6 @@ namespace util
       MPI_Comm_size(MPI_COMM_WORLD, &rank_count);
       return rank_count;
     }
-
-
-    std::mutex Tag::s_free_tag_mutex;
-    std::list<int> Tag::s_free_tags = Tag::initFreeTags(MPIUTIL_TAG_RANGE_START, MPIUTIL_TAG_RANGE_END);
 
   } // namespace mpi
 
