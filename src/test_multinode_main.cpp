d526cf7e1cabaebd081e45360e4b185aa1cbc203
ac101m <mofty101@gmail.com>
2019-11-13 16:28:52 +0000

    Add mpi initialisation and finalization to multinode_main

--- src/test_multinode_main.cpp
+++ src/test_multinode_main.cpp
@@ -1,2 +1,19 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+
+// External
+#include "mpi.h"
+
+
+// Distributed tests require that MPI is initialised, so lets do that
+int main(int argc, char** argv)
+{
+  MPI_Init(&argc, &argv);
+
+  // Run catch tests
+  int result = Catch::Session().run(argc, argv);
+
+  MPI_Finalize();
+  return result;
+}
