307fc7efdc014157e9c2e57e45dca0f54c05463e
ac101m <mofty101@gmail.com>
2019-11-18 23:19:23 +0000

    Add distributor class

---
+++ src/test_multinode/Distributor.cpp
@@ -0,0 +1,153 @@
+// This is a catch module
+#include "catch.hpp"
+
+
+// Internal
+#include "util/Distributor.hpp"
+#include "util/mpi/Transmissable.hpp"
+
+// Standard
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+
+class TransmissableInt : public util::mpi::Transmissable
+{
+private:
+  int m_value;
+
+public:
+  TransmissableInt(int const t_intial_value = -1) :
+    m_value(t_intial_value)
+  {}
+
+  friend bool operator==(TransmissableInt const & lhs, TransmissableInt const & rhs)
+  {
+    return (lhs.m_value == rhs.m_value);
+  }
+
+  friend bool operator!=(TransmissableInt const & lhs, TransmissableInt const & rhs)
+  {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator>(TransmissableInt const & lhs, TransmissableInt const & rhs)
+  {
+    return (lhs.m_value > rhs.m_value);
+  }
+
+  friend bool operator<(TransmissableInt const & lhs, TransmissableInt const & rhs)
+  {
+    return (lhs.m_value < rhs.m_value);
+  }
+
+  operator int()
+  {
+    return m_value;
+  }
+
+  void mpiSend(int const t_destination, int const t_tag, util::mpi::Communicator const t_comm) const
+  {
+    int err = MPI_Ssend(&m_value, 1, MPI_INT, t_destination, t_tag, t_comm);
+    if(err)
+    {
+      std::cout << "MPI Error, code: " << err << "\n";
+      exit(1);
+    }
+  }
+
+  void mpiReceive(int const t_source, int const t_tag, util::mpi::Communicator const t_comm)
+  {
+    MPI_Status status;
+    int err = MPI_Recv(&m_value, 1, MPI_INT, t_source, t_tag, t_comm, &status);
+    if(err)
+    {
+      std::cout << "MPI Error, code: " << err << "\n";
+      exit(1);
+    }
+  }
+};
+
+
+SCENARIO(
+  "[Distributor] - Single rank test")
+{
+  unsigned input_queue_length = 4;
+  unsigned output_queue_length = 4;
+
+  std::shared_ptr<util::Queue<TransmissableInt>> input_queue(new util::Queue<TransmissableInt>(input_queue_length));
+  std::shared_ptr<util::Queue<TransmissableInt>> output_queue(new util::Queue<TransmissableInt>(output_queue_length));
+
+  util::mpi::Communicator comm_all = util::mpi::Communicator::world();
+  util::mpi::Communicator comm = util::mpi::Communicator::split(comm_all, comm_all.rank(), comm_all.rank());
+
+  util::Distributor<TransmissableInt> distributor(input_queue, output_queue);
+
+  GIVEN("A vector of transmissable items")
+  {
+    unsigned test_vector_length = 4;
+
+    std::vector<TransmissableInt> input_vector = std::vector<TransmissableInt>(test_vector_length);
+    std::vector<TransmissableInt> output_vector = std::vector<TransmissableInt>(test_vector_length);
+
+    for(unsigned i = 0; i < input_vector.size(); i++)
+    {
+      input_vector[i] = rand();
+    }
+
+    WHEN("The vector is enqueued and dequeued on the same rank")
+    {
+      std::thread enqueue_thread(&util::Queue<TransmissableInt>::enqueueVector, &(*input_queue), std::ref(input_vector));
+      std::thread dequeue_thread(&util::Queue<TransmissableInt>::dequeueVector, &(*output_queue), std::ref(output_vector));
+
+      enqueue_thread.join();
+      dequeue_thread.join();
+
+      THEN("The values are preserved")
+      {
+        std::sort(input_vector.begin(), input_vector.end());
+        std::sort(output_vector.begin(), output_vector.end());
+
+        for(unsigned i = 0; i < input_vector.size(); i++)
+        {
+          std::cout << i << ") in: " << input_vector[i] << " out: " << output_vector[i] << "\n";
+        }
+
+        bool vectors_match = (input_vector == output_vector);
+        REQUIRE(vectors_match == true);
+      }
+    }
+  }
+}
+
+/*
+SCENARIO(
+  "[Distributor] - Parallel enqueue/dequeue test")
+{
+  unsigned input_queue_length = 4;
+  unsigned output_queue_length = 4;
+
+  std::shared_ptr<util::Queue<TransmissableInt>> input_queue(new util::Queue<TransmissableInt>(input_queue_length));
+  std::shared_ptr<util::Queue<TransmissableInt>> output_queue(new util::Queue<TransmissableInt>(output_queue_length));
+
+  util::mpi::Communicator communicator = util::mpi::Communicator::world();
+
+  util::Distributor<TransmissableInt> distributor(input_queue, output_queue, 1, 1, 1, communicator);
+
+  GIVEN("A vector of test items")
+  {
+    unsigned test_vector_length = 65536;
+
+    WHEN("The data is enqueued on one node")
+    {
+
+
+      THEN("Input data matches output data")
+      {
+
+      }
+    }
+  }
+}
+*/
---
+++ src/util/Distributor.hpp
@@ -0,0 +1,301 @@
+#ifndef MPIBROT_DISTRIBUTOR_INCLUDED
+#define MPIBROT_DISTRIBUTOR_INCLUDED
+
+
+// Standard
+#include <vector>
+#include <memory>
+#include <thread>
+#include <iostream>
+
+// External
+#include "mpi.h"
+
+// Internal
+#include "util/Queue.hpp"
+#include "util/mpi/Communicator.hpp"
+
+
+// Communication tags
+#define MPIBROT_DISTRIBUTOR_TX_REQUEST_TAG 0
+#define MPIBROT_DISTRIBUTOR_TX_RESPONSE_TAG 1
+#define MPIBROT_DISTRIBUTOR_RX_REQUEST_TAG 2
+#define MPIBROT_DISTRIBUTOR_RX_RESPONSE_TAG 3
+#define MPIBROT_DISTRIBUTOR_DATA_TAG 4
+
+// Stop signal
+#define MPIBROT_DISTRIBUTOR_STOP_SIGNAL -1
+
+
+namespace util
+{
+
+  template<class T>
+  class Distributor
+  {
+  private:
+    std::shared_ptr<util::Queue<T>> m_input_queue;
+    std::shared_ptr<util::Queue<T>> m_output_queue;
+
+    MPI_Comm m_comm_all;
+
+    std::vector<std::thread> m_signal_handler_threads;
+    std::vector<std::thread> m_transmit_threads;
+    std::vector<std::thread> m_receive_threads;
+
+    int const m_signal_group_size;
+    int const m_my_signal_group;
+    int const m_my_signal_handler_rank;
+
+    T const m_terminate_value;
+
+
+  // Methods
+  private:
+    void sendInt(int const t_destination, int const t_tag, int const t_value)
+    {
+      int err = MPI_Ssend(&t_value, 1, MPI_INT, t_destination, t_tag, m_comm_all);
+      if(err)
+      {
+        std::cout << "MPI Error, code: " << err << "\n";
+        exit(1);
+      }
+    }
+
+
+    int receiveInt(int const t_source, int const t_tag)
+    {
+      int value;
+      MPI_Status status;
+      int err = MPI_Recv(&value, 1, MPI_INT, t_source, t_tag, m_comm_all, &status);
+      if(err)
+      {
+        std::cout << "MPI Error, code: " << err << "\n";
+        exit(1);
+      }
+      return value;
+    }
+
+
+    void sendRankNumber(int const t_destination, int const t_tag)
+    {
+      this->sendInt(t_destination, t_tag, m_comm_all.rank());
+    }
+
+
+    int receiveRankNumber(int const t_source, int const t_tag)
+    {
+      return this->receiveInt(t_source, t_tag);
+    }
+
+
+    void sendRxRequest(int const t_signal_handler_rank)
+    {
+      this->sendRankNumber(t_signal_handler_rank, MPIBROT_DISTRIBUTOR_RX_REQUEST_TAG);
+    }
+
+
+    int receiveRxRequest()
+    {
+      return this->receiveRankNumber(MPI_ANY_SOURCE, MPIBROT_DISTRIBUTOR_RX_REQUEST_TAG);
+    }
+
+
+    void sendRxResponse(int const t_destination, int const t_value)
+    {
+      this->sendInt(t_destination, MPIBROT_DISTRIBUTOR_RX_RESPONSE_TAG, t_value);
+    }
+
+
+    int receiveRxResponse(int const t_source)
+    {
+      return this->receiveInt(t_source, MPIBROT_DISTRIBUTOR_RX_RESPONSE_TAG);
+    }
+
+
+    void sendTxRequest(int const t_destination)
+    {
+      this->sendRankNumber(t_destination, MPIBROT_DISTRIBUTOR_TX_REQUEST_TAG);
+    }
+
+
+    void sendTxStopRequest(int const t_destination)
+    {
+      this->sendInt(t_destination, MPIBROT_DISTRIBUTOR_TX_REQUEST_TAG, MPIBROT_DISTRIBUTOR_STOP_SIGNAL);
+    }
+
+
+    int receiveTxRequest()
+    {
+      return this->receiveRankNumber(MPI_ANY_SOURCE, MPIBROT_DISTRIBUTOR_TX_REQUEST_TAG);
+    }
+
+
+    void sendTxResponse(int const t_destination, int const t_response)
+    {
+      this->sendInt(t_destination, MPIBROT_DISTRIBUTOR_TX_RESPONSE_TAG, t_response);
+    }
+
+
+    int receiveTxResponse(int const t_source)
+    {
+      return this->receiveInt(t_source, MPIBROT_DISTRIBUTOR_TX_RESPONSE_TAG);
+    }
+
+
+    void signalHandlerMain()
+    {
+      int tx_request_rank, rx_request_rank;
+
+      while(1)
+      {
+        tx_request_rank = this->receiveTxRequest();
+
+        if(tx_request_rank == MPIBROT_DISTRIBUTOR_STOP_SIGNAL)
+        {
+          break;
+        }
+
+        rx_request_rank = this->receiveRxRequest();
+
+        this->sendTxResponse(tx_request_rank, rx_request_rank);
+        this->sendRxResponse(rx_request_rank, tx_request_rank);
+      }
+    }
+
+
+    void receiveThreadMain(int const t_signal_handler_rank)
+    {
+      T rx_data;
+      int tx_rank;
+
+      while(1)
+      {
+        this->sendRxRequest(t_signal_handler_rank);
+        tx_rank = this->receiveRxResponse(t_signal_handler_rank);
+
+        if(tx_rank == MPIBROT_DISTRIBUTOR_STOP_SIGNAL)
+        {
+          break;
+        }
+
+        rx_data.mpiReceive(tx_rank, MPIBROT_DISTRIBUTOR_DATA_TAG, m_comm_all);
+        m_output_queue->enqueue(rx_data);
+      }
+    }
+
+
+    void transmitThreadMain()
+    {
+      T tx_data;
+      int rx_rank;
+
+      while((tx_data = this->m_input_queue->dequeue()) != m_terminate_value)
+      {
+        this->sendTxRequest(m_my_signal_handler_rank);
+        rx_rank = this->receiveTxResponse(m_my_signal_handler_rank);
+        tx_data.mpiSend(rx_rank, MPIBROT_DISTRIBUTOR_DATA_TAG, m_comm_all);
+      }
+    }
+
+
+  // Methods
+  public:
+    Distributor(
+      std::shared_ptr<util::Queue<T>> t_input_queue,
+      std::shared_ptr<util::Queue<T>> t_output_queue,
+      unsigned const t_signal_group_count = 1,
+      unsigned const t_transmit_thread_count = 1,
+      unsigned const t_signal_thread_count = 1,
+      mpi::Communicator const t_basis_communicator = util::mpi::Communicator::world(),
+      T const t_terminate_value = T()) :
+      m_input_queue(t_input_queue),
+      m_output_queue(t_output_queue),
+      m_comm_all(t_basis_communicator),
+      m_signal_group_size((m_comm_all.size() + (t_signal_group_count / 2)) / t_signal_group_count),
+      m_my_signal_group(m_comm_all.rank() / m_signal_group_size),
+      m_my_signal_handler_rank(m_my_signal_group * m_signal_group_size),
+      m_terminate_value(t_terminate_value)
+    {
+      // Start transmit threads
+      for(unsigned i = 0; i < t_transmit_thread_count; i++)
+      {
+        m_transmit_threads.push_back(std::thread(&util::Distributor<T>::transmitThreadMain, this));
+      }
+
+      // Get vector of all ranks with signal handlers running on them
+      std::vector<int> signal_handler_ranks;
+      for(int i = 0; i < m_comm_all.size(); i++)
+      {
+        if((m_comm_all.rank() % m_signal_group_size) == 0)
+        {
+          signal_handler_ranks.push_back(m_comm_all.rank());
+        }
+      }
+
+      // Start one receive thread for each signal handler rank
+      // pass it the rank of the signal handler it should listen to
+      for(unsigned i = 0; i < signal_handler_ranks.size(); i++)
+      {
+        m_receive_threads.push_back(std::thread(&util::Distributor<T>::receiveThreadMain, this, signal_handler_ranks.at(i)));
+      }
+
+      // Start signal handlers
+      if(m_comm_all.rank() == m_my_signal_handler_rank)
+      {
+        for(unsigned i = 0; i < t_signal_thread_count; i++)
+        {
+          m_signal_handler_threads.push_back(std::thread(&util::Distributor<T>::signalHandlerMain, this));
+        }
+      }
+    }
+
+
+    ~Distributor()
+    {
+      // Send stop signals to transmit threads
+      for(unsigned i = 0; i < m_transmit_threads.size(); i++)
+      {
+        this->m_input_queue->enqueue(m_terminate_value);
+      }
+
+      // Join transmit threads
+      for(unsigned i = 0; i < m_transmit_threads.size(); i++)
+      {
+        m_transmit_threads.at(i).join();
+      }
+
+      // Send stop signals to signal handlers on this rank
+      for(unsigned i = 0; i < m_signal_handler_threads.size(); i++)
+      {
+        this->sendTxStopRequest(m_comm_all.rank());
+      }
+
+      // Join signal handler threads
+      for(unsigned i = 0; i < m_signal_handler_threads.size(); i++)
+      {
+        m_signal_handler_threads.at(i).join();
+      }
+
+      // Send stop signals to receive threads
+      if(m_comm_all.rank() == m_my_signal_handler_rank)
+      {
+        for(int i = 0; i < m_comm_all.size(); i++)
+        {
+          int rx_rank = this->receiveRxRequest();
+          this->sendRxResponse(rx_rank, MPIBROT_DISTRIBUTOR_STOP_SIGNAL);
+        }
+      }
+
+      // Join receive threads
+      for(unsigned i = 0; i < m_receive_threads.size(); i++)
+      {
+        m_receive_threads.at(i).join();
+      }
+    }
+  };
+
+} // namespace util
+
+
+#endif // MPIBROT_DISTRIBUTOR_INCLUDED
