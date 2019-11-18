0d5159dfd884db9a3775cf41a57418958107570a
ac101m <mofty101@gmail.com>
2019-11-18 14:51:45 +0000

    So that's where all those race conditions came from...

--- src/test_multinode_main.cpp
+++ src/test_multinode_main.cpp
@@ -9,7 +9,16 @@
 // Distributed tests require that MPI is initialised, so lets do that
 int main(int argc, char** argv)
 {
-  MPI_Init(&argc, &argv);
+  int thread_level_required = MPI_THREAD_MULTIPLE;
+  int thread_level_actual;
+
+  MPI_Init_thread(&argc, &argv, thread_level_required, &thread_level_actual);
+
+  if(thread_level_actual != thread_level_required)
+  {
+    std::cout << "Error, could not initialise threaded MPI environment\n";
+    exit(1);
+  }
 
   // Run catch tests
   int result = Catch::Session().run(argc, argv);
