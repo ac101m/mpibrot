233414e850d498687eb5a29e85bc7d7ea7d4d8bf
ac101m <mofty101@gmail.com>
2019-11-19 18:44:38 +0000

    Add signals to queue class

--- src/util/Queue.hpp
+++ src/util/Queue.hpp
@@ -15,7 +15,7 @@
 namespace util
 {
 
-  // Base class for things that you can Enqueue stuff to
+  // Base class for things that you can enqueue stuff to
   template<class T>
   class Enqueue
   {
@@ -38,7 +38,7 @@ namespace util
   {
   public:
     virtual T dequeue() = 0;
-    
+
     void dequeueVector(std::vector<T> & t_data)
     {
       for(unsigned i = 0; i < t_data.size(); i++)
@@ -55,48 +55,67 @@ namespace util
   private:
     unsigned m_front = 0;
     unsigned m_back = 0;
-    std::vector<T> m_buffer;
+    std::vector<std::pair<int, T>> m_buffer;
     std::unique_ptr<util::CountingSemaphore> m_sem_data;
     std::unique_ptr<util::CountingSemaphore> m_sem_free_space;
     std::mutex m_mutex;
+
 
   public:
     Queue(unsigned const t_size) :
       m_front(0),
       m_back(0),
-      m_buffer(std::vector<T>(t_size)),
+      m_buffer(std::vector<std::pair<int, T>>(t_size, std::make_pair(0, T()))),
       m_sem_data(new util::CountingSemaphore(0)),
       m_sem_free_space(new util::CountingSemaphore(t_size))
     {}
 
-    void enqueue(T t_data)
+
+    inline void enqueueWithSignal(std::pair<int, T> const t_signal_data_pair)
     {
       m_sem_free_space->take();
 
       {
         std::lock_guard<decltype(m_mutex)> lock(m_mutex);
-        m_buffer[m_back] = t_data;
+        m_buffer[m_back] = t_signal_data_pair;
         m_back = (m_back + 1) % m_buffer.size();
       }
 
       m_sem_data->give();
     }
 
-    T dequeue()
+
+    inline void enqueue(T const t_data)
+    {
+      std::pair<int, T> signal_data_pair = std::make_pair(0, t_data);
+      this->enqueueWithSignal(signal_data_pair);
+    }
+
+
+    inline std::pair<int, T> dequeueWithSignal()
     {
       m_sem_data->take();
 
-      T data;
+      std::pair<int, T> signal_data_pair;
       {
         std::lock_guard<decltype(m_mutex)> lock(m_mutex);
-        data = m_buffer[m_front];
-        m_buffer[m_front] = T();
+        signal_data_pair = m_buffer[m_front];
+        m_buffer[m_front] = std::make_pair(0, T());
         m_front = (m_front + 1) % m_buffer.size();
       }
 
       m_sem_free_space->give();
-      return data;
+
+      return signal_data_pair;
     }
+
+
+    inline T dequeue()
+    {
+      std::pair<int, T> signal_data_pair = this->dequeueWithSignal();
+      return signal_data_pair.second;
+    }
+
 
     unsigned size() const
     {
